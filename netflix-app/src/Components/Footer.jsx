cc7e619c1eccd5fdcfcf26d08b5bf7598298fe81
MelissaCanu <melissamcanu@gmail.com>
2023-12-22 14:55:31 +0100

    starting footer component

---
+++ netflix-app/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { BiFacebook, BiInstagram, BiTwitterX, BiYoutube } from "react-icons/bi";
+
+const Footer = () => {
+     return ()
+}
+
+export default Footer
